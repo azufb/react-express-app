9a3063c6f101fa75195cdeab5a39d9a8d7fb6220
azufb <curiousazunas@gmail.com>
2023-07-17 18:09:48 +0900

    ルーティング追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,13 @@
-import { Routes, Route, Link, Outlet, BrowserRouter } from 'react-router-dom';
-import Signup from './pages/Auth/Signup';
-import Signin from './pages/Auth/Signin';
-import TasksIndex from './pages/tasks/TasksIndex';
-import GreetingIndex from './pages/greeting/GreetingIndex';
+import Router from './Router';
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from './apolloClient';
-
-const Layout = () => {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/Signup'>サインアップ</Link>
-          </li>
-          <li>
-            <Link to='/Signin'>サインイン</Link>
-          </li>
-          <li>
-            <Link to='/Tasks'>タスク</Link>
-          </li>
-          <li>
-            <Link to='/Greeting'>Greeting</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
-    </div>
-  );
-};
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <ApolloProvider client={apolloClient}>
-          <Routes>
-            <Route path='/' element={<Layout />}>
-              <Route path='/Signup' element={<Signup />} />
-              <Route path='/Signin' element={<Signin />} />
-              <Route path='/Tasks' element={<TasksIndex />} />
-              <Route path='/Greeting' element={<GreetingIndex />} />
-            </Route>
-          </Routes>
-        </ApolloProvider>
-      </BrowserRouter>
+      <ApolloProvider client={apolloClient}>
+        <Router />
+      </ApolloProvider>
     </div>
   );
 };
