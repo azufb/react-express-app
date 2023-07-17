99eabf72740c118cfe14adcdfe2131bede2666a4
azufb <curiousazunas@gmail.com>
2023-07-17 21:37:43 +0900

    編集モード切り替え実装

--- src/pages/tasks/components/TaskList.tsx
+++ src/pages/tasks/components/TaskList.tsx
@@ -1,5 +1,8 @@
 import { useMutation, useQuery } from '@apollo/client';
 import { DELETE_TASK, GET_TASKS } from '../../../ts/gql';
+import { useRecoilState } from 'recoil';
+import { isTaskEditableAtom } from '../../../recoil/atom';
+import EditTask from './EditTask';
 
 type DeleteParam = {
   id: number;
@@ -11,6 +14,14 @@ const TaskList = () => {
     // 削除が実行されたら、再度タスク一覧を取得する
     refetchQueries: [GET_TASKS, 'GetTasks'],
   });
+
+  const [isTaskEditable, setIsTaskEditableAtom] =
+    useRecoilState<boolean>(isTaskEditableAtom);
+
+  // 編集モードにする
+  const handleChangeEditable = (): void => {
+    setIsTaskEditableAtom(true);
+  };
 
   // 削除ボタン押下で、タスク削除処理を実行
   const handleDeleteTask = (id: number): void => {
@@ -37,6 +48,7 @@ const TaskList = () => {
                 <th>タイトル</th>
                 <th>期限</th>
                 <th></th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
@@ -44,8 +56,21 @@ const TaskList = () => {
               {data.getTasks.map((task: any) => (
                 <tr key={task.id}>
                   <td>{task.id}</td>
-                  <td>{task.title}</td>
-                  <td>{task.deadline}</td>
+                  {isTaskEditable ? (
+                    <EditTask
+                      taskId={task.id}
+                      previousTitle={task.title}
+                      previousDeadline={task.deadline}
+                    />
+                  ) : (
+                    <>
+                      <td>{task.title}</td>
+                      <td>{task.deadline}</td>
+                      <td>
+                        <button onClick={handleChangeEditable}>編集</button>
+                      </td>
+                    </>
+                  )}
                   <td>
                     <button onClick={() => handleDeleteTask(task.id)}>
                       削除
