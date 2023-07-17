f79d223bb8e669865f76449723489283cbcd499c
azufb <curiousazunas@gmail.com>
2023-07-17 19:30:53 +0900

    認証状態に応じたナビゲーション実装

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,18 @@
-import { Routes, Route, Link, Outlet, BrowserRouter } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  BrowserRouter,
+  Navigate,
+} from 'react-router-dom';
 import Signup from './pages/Auth/Signup';
 import Signin from './pages/Auth/Signin';
 import TasksIndex from './pages/tasks/TasksIndex';
 import GreetingIndex from './pages/greeting/GreetingIndex';
+import HomeIndex from './pages/Home/HomeIndex';
+import { useRecoilValue } from 'recoil';
+import { isSignedinAtom } from './recoil/atom';
 
 const Layout = () => {
   return (
@@ -11,12 +21,6 @@ const Layout = () => {
         <ul>
           <li>
             <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/Signup'>サインアップ</Link>
-          </li>
-          <li>
-            <Link to='/Signin'>サインイン</Link>
           </li>
           <li>
             <Link to='/Tasks'>タスク</Link>
@@ -34,15 +38,39 @@ const Layout = () => {
   );
 };
 
+const RequireAuth = ({ children }: { children: JSX.Element }) => {
+  const isSignedin = useRecoilValue<boolean>(isSignedinAtom);
+  if (isSignedin) {
+    return children;
+  } else {
+    return <Navigate to='/' replace />;
+  }
+};
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
+          <Route index element={<HomeIndex />} />
           <Route path='/Signup' element={<Signup />} />
           <Route path='/Signin' element={<Signin />} />
-          <Route path='/Tasks' element={<TasksIndex />} />
-          <Route path='/Greeting' element={<GreetingIndex />} />
+          <Route
+            path='/Tasks'
+            element={
+              <RequireAuth>
+                <TasksIndex />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path='/Greeting'
+            element={
+              <RequireAuth>
+                <GreetingIndex />
+              </RequireAuth>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
