ea94fe6ea666fe6effd1e7c8b20abcd980879651
azufb <curiousazunas@gmail.com>
2023-07-17 12:48:44 +0900

    グリーティング・タスク管理コンポーネント表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { gql, useQuery } from '@apollo/client';
+import GreetingIndex from './pages/greeting/GreetingIndex';
 import TasksIndex from './pages/tasks/TasksIndex';
 
-const GET_GREETING = gql`
-  query getGreeting {
-    hello
-    goodbye
-  }
-`;
-
 const App = () => {
-  const { loading, error, data } = useQuery(GET_GREETING);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
-
   return (
     <div>
-      <header>
-        <p>{data.hello}</p>
-        <p>{data.goodbye}</p>
-      </header>
+      <GreetingIndex />
       <TasksIndex />
     </div>
   );
