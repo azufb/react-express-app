bbaf476f6586cc98a31a8d93cbab03c9fff6259f
azufb <curiousazunas@gmail.com>
2023-07-17 20:34:54 +0900

    Homeコンポーネント修正

--- src/pages/Home/HomeIndex.tsx
+++ src/pages/Home/HomeIndex.tsx
@@ -1,24 +1,7 @@
-import { useRecoilValue } from 'recoil';
-import SigninButton from './components/SigninButton';
-import SignupButton from './components/SignupButton';
-import { isSignedinAtom } from '../../recoil/atom';
-
 const HomeIndex = () => {
-  const isSignedin = useRecoilValue<boolean>(isSignedinAtom);
-
   return (
     <div>
       <p>Home</p>
-      {isSignedin ? (
-        <>
-          <p>初めて利用する方はこちら</p>
-          <SignupButton />
-          <p>サインインはこちら</p>
-          <SigninButton />
-        </>
-      ) : (
-        <p>サインインしています。</p>
-      )}
     </div>
   );
 };
