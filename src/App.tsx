961330a660d40a99cd9af8a587595d439728ba15
azufb <curiousazunas@gmail.com>
2023-07-17 09:37:57 +0900

    query修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { gql, useQuery } from '@apollo/client';
 import TasksIndex from './pages/tasks/TasksIndex';
 
-const GET_HELLO = gql`
-  query getHello {
+const GET_GREETING = gql`
+  query getGreeting {
     hello
+    goodbye
   }
 `;
 
 const App = () => {
-  const { loading, error, data } = useQuery(GET_HELLO);
+  const { loading, error, data } = useQuery(GET_GREETING);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
@@ -17,6 +18,7 @@ const App = () => {
     <div>
       <header>
         <p>{data.hello}</p>
+        <p>{data.goodbye}</p>
       </header>
       <TasksIndex />
     </div>
