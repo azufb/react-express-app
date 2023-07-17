7538d01e4af9faac71f7dfcbaae36ce149527422
azufb <curiousazunas@gmail.com>
2023-07-17 12:47:54 +0900

    gqlをまとめたtsファイル作成

---
+++ src/ts/gql/index.ts
@@ -0,0 +1,38 @@
+import { DocumentNode, gql } from '@apollo/client';
+
+export const GET_GREETING: DocumentNode = gql`
+  query getGreeting {
+    hello
+    goodbye
+  }
+`;
+
+export const ADD_TASK: DocumentNode = gql`
+  mutation AddTask($title: String, $deadline: String) {
+    addTask(title: $title, deadline: $deadline) {
+      id
+      title
+      deadline
+    }
+  }
+`;
+
+export const GET_TASKS: DocumentNode = gql`
+  query GetTasks {
+    getTasks {
+      id
+      title
+      deadline
+    }
+  }
+`;
+
+export const DELETE_TASK: DocumentNode = gql`
+  mutation DeleteTask($id: Int) {
+    deleteTask(id: $id) {
+      id
+      title
+      deadline
+    }
+  }
+`;
