7990a9f36bcb483100e7cc750c3256694dc1e8e3
azufb <curiousazunas@gmail.com>
2023-07-18 23:03:47 +0900

    App.tsxにスタイル適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+/** @jsxImportSource @emotion/react */
+
 import Router from './Router';
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from './apolloClient';
+import { appTitle } from './styles/app';
 
 const App = () => {
   return (
     <div>
       <ApolloProvider client={apolloClient}>
+        <h1 css={appTitle}>GraphQLに挑戦！</h1>
         <Router />
       </ApolloProvider>
     </div>
---
+++ src/styles/app.ts
@@ -0,0 +1,5 @@
+import { css } from '@emotion/react';
+
+export const appTitle = css`
+  text-align: center;
+`;
