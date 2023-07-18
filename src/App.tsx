15c6d1a0b1ced5d6935b13a73a0ce8fb6828da92
azufb <curiousazunas@gmail.com>
2023-07-18 23:48:41 +0900

    全体のpadding調整

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@
 import Router from './Router';
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from './apolloClient';
-import { appTitle } from './styles/app';
+import { appTitle, appWrapper } from './styles/app';
 
 const App = () => {
   return (
-    <div>
+    <div css={appWrapper}>
       <ApolloProvider client={apolloClient}>
         <h1 css={appTitle}>GraphQLに挑戦！</h1>
         <Router />
--- src/styles/app.ts
+++ src/styles/app.ts
@@ -1,5 +1,10 @@
 import { css } from '@emotion/react';
 
+export const appWrapper = css`
+  padding: 1.5rem 2rem;
+`;
+
 export const appTitle = css`
   text-align: center;
+  margin: 0;
 `;
