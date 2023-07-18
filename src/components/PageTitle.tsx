c1bcccdab5fcb18832b719888a6c8891b4970b79
azufb <curiousazunas@gmail.com>
2023-07-18 23:26:10 +0900

    PageTitleコンポーネントにスタイル適用

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,9 +1,12 @@
+/** @jsxImportSource @emotion/react */
+import { pageTitle } from './styles/pageTitle';
+
 type PropsType = {
   pageTitle: string;
 };
 
 const PageTitle = (props: PropsType) => {
-  return <h2>{props.pageTitle}</h2>;
+  return <h2 css={pageTitle}>{props.pageTitle}</h2>;
 };
 
 export default PageTitle;
