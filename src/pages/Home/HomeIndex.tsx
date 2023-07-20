e8927b19e6690a6e518c6157f0b93c670e09909b
azufb <curiousazunas@gmail.com>
2023-07-20 22:57:57 +0900

    トップページ

--- src/pages/Home/HomeIndex.tsx
+++ src/pages/Home/HomeIndex.tsx
@@ -1,11 +1,20 @@
+/** @jsxImportSource @emotion/react */
+
 import Layout from '../../components/Layout';
 import PageTitle from '../../components/PageTitle';
+import { contentsWrapper, text } from './styles/homeIndex';
 
 const HomeIndex = () => {
   return (
     <Layout>
       <PageTitle pageTitle='Home' />
-      <p>ああああああああああああああ</p>
+      <div css={contentsWrapper}>
+        <p css={text}>
+          Express × GraphQL × Reactで実装してみた！
+          <br />
+          Prismaも使ってみた！
+        </p>
+      </div>
     </Layout>
   );
 };
---
+++ src/pages/Home/styles/homeIndex.ts
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react';
+
+export const contentsWrapper = css`
+  width: 800px;
+  margin: 0 auto;
+`;
+
+export const text = css`
+  text-align: center;
+`;
